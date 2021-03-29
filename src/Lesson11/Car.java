7d61dc924bec1696929a9a0db414d6c693fca63b
Bashnya202 <Bashnya202@gmail.com>
2021-03-29 20:22:43 +0300

    123

--- src/Lesson11/Car.java
+++ src/Lesson11/Car.java
@@ -5,40 +5,37 @@ public class Car {
     String engine;
     int kolDoor;
 
-    Car (String color, String engine, int kolDoor){
-        this.color=color;
-        this.engine=engine;
-        this.kolDoor=kolDoor;
+    Car(String color, String engine, int kolDoor) {
+        this.color = color;
+        this.engine = engine;
+        this.kolDoor = kolDoor;
     }
 }
 
 
 class CarTest {
 
-    void numberDoor (Car x, int kolDoor){
-        x.kolDoor=kolDoor;
+    void numberDoor(Car x, int kolDoor) {
+        x.kolDoor = kolDoor;
     }
 
-    static void changeColor (Car car1, Car car2){
+    static void changeColor(Car car1, Car car2) {
         String color = car1.color;
         car1.color = car2.color;
-        car2.color= color;
+        car2.color = color;
 
     }
-
 
 
     public static void main(String[] args) {
-        Car c1 = new Car("White","V6",5);
+        Car c1 = new Car("White", "V6", 5);
         Car c2 = new Car("Black", "V8", 3);
         CarTest ct1 = new CarTest();
         System.out.println(c1.kolDoor);
-        ct1.numberDoor(c1,3);
+        ct1.numberDoor(c1, 3);
         System.out.println(c1.kolDoor);
-        changeColor(c1,c2);
-        System.out.println(c1.color+" "+c2.color);
-
-        System.out.println(c1.kolDoor);
+        changeColor(c1, c2);
+        System.out.println(c1.color + " " + c2.color);
     }
 
 
